792cd237b0e62679c91b0304bf5d279cb786509a
Cupelt <guswnd8800@naver.com>
2023-10-13 14:33:51 +0900

    yo

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const io = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
-        const child = entry.target.children
-
-        for(i = 0; i < child.length; i++) {
-            if(entry.isIntersecting && child[i].className.split(" ").indexOf("scroll_animation") > -1) {
-                child[i].classList.add('trigger_animation');
-
-                for(j = 0; j < child[i].children.length; j++) {
-                    if (child[i].children[j].className.split(" ").indexOf("scroll_animation") > -1)
-                        child[i].children[j].classList.add('trigger_animation');
-                }
-            } else {
-                child[i].classList.remove('trigger_animation');
-                
-                for(j = 0; j < child[i].children.length; j++) {
-                    if (child[i].children[j].className.split(" ").indexOf("scroll_animation") > -1)
-                        child[i].children[j].classList.remove('trigger_animation');
-                }
-            }
+        if(entry.isIntersecting) {
+            addAnimation(entry.target, 0)
+        } else {
+            removeAnimation(entry.target)
         }
     });
 });
+
+function addAnimation(object, ani_counter) {
+    if (object.className.split(" ").indexOf("scroll_animation") > -1) {
+        object.style.cssText += " --delay: " + (ani_counter * 50) + "ms;"
+        object.classList.add('trigger_animation');
+    }
+
+    if (object.children.length > 0) {
+        for(let i = 0; i < object.children.length; i++) {
+            ani_counter += 3;
+            addAnimation(object.children[i], ani_counter)
+        }
+    }
+}
+
+function removeAnimation(object) {
+    if (object.className.split(" ").indexOf("scroll_animation") > -1) {
+        object.classList.remove('trigger_animation');
+    }
+    
+    if (object.children.length > 0) {
+        for(let i = 0; i < object.children.length; i++) {
+            removeAnimation(object.children[i])
+        }
+    }
+}
 
 document.querySelectorAll('.chapter').forEach((element) => io.observe(element));
